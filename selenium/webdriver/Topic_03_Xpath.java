caa085a9951ea0e45fad95645b003d9422663ae9
thienbinh <thienbinhfm956@gmail.com>
2022-04-07 06:29:32 +0700

    commit topic 03 xpath

--- selenium/webdriver/Topic_03_Xpath.java
+++ selenium/webdriver/Topic_03_Xpath.java
@@ -1,8 +1,11 @@
 package webdriver;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -23,7 +26,33 @@ public class Topic_03_Xpath {
 
 	@Test
 	public void TC_01_() {
-		
+		 // Gồm có 3 bước 
+		// Tìm (find) - số ít - trả về 1 cái 
+		// Thao tác trực tiếp ko khai báo biến - sử dụng 1 lần / ko dùng lại element này
+		driver.findElement(By.id("")).clear();
+		driver.findElement(By.id("")).click();
+		driver.findElement(By.id("")).isDisplayed();
+
+		//  Khai báo biến - dùng lại Element nhiều lần
+		WebElement loginButton = driver.findElement(By.id(""));
+		loginButton.click();
+		loginButton.isDisplayed();
+
+		// Tìm (find) - số nhiều - trả về 1 hoặc >1
+		driver.findElements(By.id("")).size();
+
+		// Lặp lại nhiều lần
+		List<WebElement> loginCheckboxes = driver.findElements(By.id(""));
+
+		 // Thao tác (action) : click/type/select/hover/..
+
+		 // Kiểm tra(Verify/Assert):getText/getAttribute/getCss/..
+
+		 // Thao tác với Email Textbox
+
+		 // Thao tác với Password Textbox
+
+		 // Thao tác Login
 	}
 
 	@Test
