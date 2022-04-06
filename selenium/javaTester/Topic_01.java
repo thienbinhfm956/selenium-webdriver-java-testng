4aec7d8fddeb374d42311da1a3eeed4906cd365c
thienbinh <thienbinhfm956@gmail.com>
2022-04-07 06:37:00 +0700

    commit first topic java 01

---
+++ selenium/javaTester/Topic_01.java
@@ -0,0 +1,76 @@
+package javaTester;
+
+public class Topic_01 {
+	// Khai báo biến : Kiểu dữ liệu + Tên biến 
+	//int studentNumber;
+
+	// Khởi tạo dữ liệu 
+	//studentNumber = 100;
+
+	// Khai báo + khởi tạo 
+	//int teacherNumber = 20;
+	  //  Kiểu dữ liệu 
+	 // 1. Nguyên thủy (Primitive)
+
+	// boolean : luận lí / logic - 2 giá trị (true/false)
+	 //boolean studentSex = true;
+
+	// Số nguyên - không có phần thập phân
+	// byte 
+	//byte bEmployee = 10;
+
+	// short
+	//short sEmployee = 10;
+
+	// int 
+	//int iEmployee = 10;
+
+	// long
+	//long lEmployee = 10000;
+
+	// Số thực -  có phần thập phân
+
+	// float 
+	//float fEmployee = 7.5f;
+
+	// double
+	//double dEmployee = 8.4d;
+
+	// char : kí tự 
+	//char a = "B";
+
+	// 2. Tham chiếu (Reference)
+	 // Array 
+	//int[] studentNumbers = { 15,50,-7,19 };
+	//String[] studentName = { "Nguyễn Văn Nam","Ngô Văn Tới" };
+
+	// Interface / Class
+	//WebDriver driver = new ChromeDriver(); // Kiểu Interface
+
+	//Action action = new Actions(driver);  // Kiểu Class
+
+	//Collection : List(ArrayList / LinkedList) / Set / Queue
+	//ArrayList<String> studentCountry = new ArrayList<String>();
+
+	//Object 
+	//Object phone;
+
+	// chuỗi kí tự : Số / Chữ / Kí tự đặc biệt 
+	//String studentName = "B";
+	//String companyName = "Hoang Gia An 2022 @Copyright";
+
+	// test
+	// String a = "Hoang";
+	 //System.out.println(a);
+
+	//String b = a;
+	//System.out.println(a);
+	//System.out.println(b);
+
+	/*b = "An";
+	a = b ;
+	System.out.println(a);
+	System.out.println(b);*/
+
+}
+
